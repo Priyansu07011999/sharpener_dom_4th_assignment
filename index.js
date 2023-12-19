3c10cfd51a222e26d49d479f543dc00e51e5cbfa
Priyansu07011999 <147058839+Priyansu07011999@users.noreply.github.com>
2023-12-19 11:09:06 +0530

    4th assignment of dom

---
+++ index.js
@@ -0,0 +1,5 @@
+const fruitList = document.getElementsByClassName('fruit');
+fruitList[2].style.backgroundColor = 'yellow';
+for(let i=0; i<fruitList.length;i++){
+  fruitList[i].style.fontWeight = 'bold';
+}
